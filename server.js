eeab52d74d2254d3db51ce339bf349ddadcd13c9
Eduardo Santos <eduardo@eduardosan.com>
2017-10-19 21:28:14 -0200

    Servidor Genérico de conexão ao node

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var CONTACTS_COLLECTION = "contacts";
+
+var app = express();
+app.use(bodyParser.json());
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
+
+
+
